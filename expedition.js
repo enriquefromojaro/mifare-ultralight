3f9f88d5757dcfa39fea0f775a20bc725e5a92cd
Enrique <enriquefromojaro@gmail.com>
2018-03-10 21:03:18 +0100

    Complete expedition

--- expedition.js
+++ expedition.js
@@ -4,14 +4,16 @@ var MASTER_KEY = new ByteString('0A1A2A3A4A5A6A7A8B9BABBBCBDBEBFB', HEX);
 
 var card = new Card();
 // writting access
-var resp = card.writeFile(4, new ByteString('11SI', ASCII));
+var access = '11SI';
+var resp = card.writeFile(4, new ByteString(access, ASCII));
 if(resp.status !== '9000'){
     print('[ERROR] Error when writing acces to the event: ' + resp.status);
     exit;
 }
 
 // Writing Event
-resp = card.writeFile(5, new ByteString('EE01', ASCII));
+var event = 'EE01'
+resp = card.writeFile(5, new ByteString(event, ASCII));
 if(resp.status !== '9000'){
     print('[ERROR] Error when writing event code: ' + resp.status);
     exit();
@@ -25,15 +27,17 @@ if(resp.status !== '9000'){
     exit();
 }
 //Writing Vendor
-resp = card.writeFile(7, new ByteString('VD01', ASCII));
+var vendor = 'VD01';
+resp = card.writeFile(7, new ByteString(vendor, ASCII));
 if(resp.status !== '9000'){
     print('[ERROR] Error when writing vendor: ' + resp.status);
-    exit;
+    exit();
 }
 
 // writing grandstand
 var grandstand = 25;
-resp = card.writeFile(8, new ByteString(Utils.numbers.fixedLengthIntString(grandstand, 4), ASCII));
+grandStand = Utils.numbers.fixedLengthIntString(grandstand, 4)
+resp = card.writeFile(8, new ByteString(grandStand, ASCII));
 if(resp.status !== '9000'){
     print('[ERROR] Error when writing grandstand: ' + resp.status);
     exit();
@@ -41,21 +45,27 @@ if(resp.status !== '9000'){
 
 // writting year
 var year = 2018;
-resp = card.writeFile(9, new ByteString(Utils.numbers.fixedLengthIntString(year, 4), ASCII));
+year = Utils.numbers.fixedLengthIntString(year, 4)
+resp = card.writeFile(9, new ByteString(year, ASCII));
 if(resp.status !== '9000'){
     print('[ERROR] Error when writing year: ' + resp.status);
-    exit;
+    exit();
 }
 
 //writing date
-resp = card.writeFile(0x0A, new ByteString('1203', ASCII));
+var date = '1203';
+resp = card.writeFile(0x0A, new ByteString(date, ASCII));
 if(resp.status !== '9000'){
     print('[ERROR] Error when writing date: ' + resp.status);
-    exit;
+    exit();
 }
 
 //writing MAC
-resp = card.writeFile(0x0B, new ByteString('1203', ASCII));
+var cardKey = card.getCardKey(MASTER_KEY);
+var macChain = access + seatString + seatString + vendor + grandstand + year + date;
+var mac = card.calcMAC(new ByteString(macChain, ASCII), cardKey);
+
+resp = card.writeFile(0x0B, mac);
 if(resp.status !== '9000'){
     print('[ERROR] Error when writing MAC: ' + resp.status);
     exit;
